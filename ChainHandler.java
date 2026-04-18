c959fd865fe6faa4f8b804383b23f145213fc140
beiserbedard <beiserbedard@cdzhongguan.com>
2026-04-19 01:28:46 +0800

    Create ChainHandler.java

---
+++ ChainHandler.java
@@ -0,0 +1,26 @@
+package chain.api;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import chain.core.BlockchainCore;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class ChainHandler implements HttpHandler {
+    private final BlockchainCore blockchain;
+
+    public ChainHandler(BlockchainCore blockchain) {
+        this.blockchain = blockchain;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = "{\"status\":\"success\",\"blocks\":" + blockchain.getChain().size() + "}";
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+}
